55d0ba7c602e520e0d523312b3517ea57f25d67e
MayankSingh0402 <mayanksingh80063@gmail.com>
2024-09-06 17:36:25 +0530

    chatting app

---
+++ frontend/src/constants/color.js
@@ -0,0 +1,10 @@
+export const orange = "#349AFF";
+export const orangeLight = "#CDE7FF";
+
+export const grayColor = "rgba(247,247,247,1)";
+export const lightBlue = "#2694ab";
+export const matBlack = "#1c1c1c";
+export const bgGradient = "linear-gradient(rgb(255 225 209), rgb(249 159 159))";
+
+export const purple = "rgba(75,12,192,1)";
+export const purpleLight = "rgba(75,12,192,0.2)";
